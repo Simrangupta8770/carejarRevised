2c4dbac57cc6ab3e8e1e8ebd67110dbf258f6aa0
Simran <simrangupta8770@gmail.com>
2023-12-08 20:00:05 +0530

    re revised

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,14 +3,14 @@ import React from 'react';
 const Navbar = () => {
   return (
     <div style={{display:'flex',padding:'5px 10px',justifyContent:'space-between',background:"black",color:'white'}}>
-        <div>
+        <div style={{cursor:'pointer',padding:'5px 10px'}}>
         App
         </div>
         <div style={{display:'flex',padding:'5px 10px'}}>
-            <div>
+            <div style={{cursor:'pointer',padding:'5px 10px'}}>
                 Link1 
             </div>
-            <div>
+            <div style={{cursor:'pointer',padding:'5px 10px'}}>
                 Link2
             </div>
         </div>
